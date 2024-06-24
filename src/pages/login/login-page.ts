17eedebace2db9261c0e8a34e7610ff485c07b7a
Florentino Carrera <florentinocarrera44@gmail.com>
2024-06-24 03:57:23 +0200

    Formulario para pantalla de login.

--- src/pages/login/login-page.ts
+++ src/pages/login/login-page.ts
@@ -1,18 +1,35 @@
 import { html, LitElement } from 'lit';
 import { PageController } from '@open-cells/page-controller';
 import { customElement } from 'lit/decorators.js';
+import { LocalizeMixin } from '@open-cells/localize';
+import '@material/web/textfield/outlined-text-field.js';
+import '@material/web/button/filled-button.js';
 
+// @ts-ignore
 @customElement('login-page')
-export class LoginPage extends LitElement {
+export class LoginPage extends LocalizeMixin(LitElement) {
   pageController = new PageController(this);
 
   protected createRenderRoot(): HTMLElement | DocumentFragment {
+    // @ts-ignore
     return this;
   }
 
   render() {
     return html`
-      <button @click="${() => this._onSubmit()}">Login</button>
+      <div class="conta">
+        <form class="form">
+          <md-outlined-text-field label="${this.t('user-label')}">
+          </md-outlined-text-field>
+
+          <md-outlined-text-field label="${this.t('password-label')}">
+          </md-outlined-text-field>
+
+          <md-filled-button @click="${() => this._onSubmit()}">
+            ${this.t('login-button')}
+          </md-filled-button>
+        </form>
+      </div>
     `;
   }
 
